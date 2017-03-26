511153880f69d753bdfe20e139f60ccbff116013
Xavier Craft <xcnetworking@gmail.com>
2017-03-26 18:51:52 -0400

    disabled screen rotation

--- PlayList/MainController.swift
+++ PlayList/MainController.swift
@@ -2,7 +2,7 @@
 //  ScoresController.swift
 //  PlayList
 //
-//  Created by Anthony Bond on 3/20/17.
+//  Created by Xavier Craft on 3/20/17.
 //  Copyright © 2017 craftx17. All rights reserved.
 //
 
@@ -15,16 +15,19 @@ class ScoresController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         view.backgroundColor = UIColor.darkGray
+        
+        let defaults = UserDefaults.standard
+        let prefs = defaults.value(forKey: "preferences") as! [String]
         
         var index: Int = 0
         var size: Int = 0
         
-        for _ in _preferences {
+        for _ in prefs {
             let button = UIButton()
             button.backgroundColor = UIColor(red: 249/255, green: 249/255, blue: 249/255, alpha: 1.0)
-            button.setTitle(_preferences[index], for: .normal)
+            button.setTitle(prefs[index], for: .normal)
             button.setTitleColor(UIColor.darkGray, for: .normal)
             button.addTarget(self, action: #selector(showTimeline), for: .touchUpInside)
             button.frame = CGRect(x: 0, y: size, width: Int(view.frame.size.width), height: 50)
@@ -36,12 +39,12 @@ class ScoresController: UIViewController {
         print(_preferences)
     }
     
-    func showTimeline() {
+    func showTimeline(sender: UIButton!) {
         // Create an API client and data source to fetch Tweets for the timeline
         let client = TWTRAPIClient()
         
         // Create the timeline view controller
-        let timelineViewController = TWTRTimelineViewController(dataSource: TWTRSearchTimelineDataSource.init(searchQuery: "\(_preferences[0]) filter:verified", apiClient: client, languageCode: "en", maxTweetsPerRequest: 500))
+        let timelineViewController = TWTRTimelineViewController(dataSource: TWTRSearchTimelineDataSource.init(searchQuery: "\(sender.currentTitle!) filter:verified", apiClient: client, languageCode: "en", maxTweetsPerRequest: 500))
         // Create done button to dismiss the view controller
         let barButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissTimeline))
         timelineViewController.navigationItem.leftBarButtonItem = barButton
@@ -57,6 +60,11 @@ class ScoresController: UIViewController {
     
     @IBAction func changePrefs(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+        _preferences.removeAll()
+        let defaults = UserDefaults.standard
+        defaults.set(_preferences, forKey: "preferences")
+        let result = defaults.value(forKey: "preferences")
+        print(result!)
     }
     
     func dismissTimeline() {
@@ -70,15 +78,11 @@ class ScoresController: UIViewController {
         set { _preferences = newValue }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
     }
-    */
-
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
 }
