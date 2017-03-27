75d9252ee87909fa026bf79fa7d884e718c256ba
Xavier Craft <xcnetworking@gmail.com>
2017-03-27 04:12:46 -0400

    Update MainController.swift

--- PlayList/MainController.swift
+++ PlayList/MainController.swift
@@ -1,81 +1,137 @@
 //
-//  ScoresController.swift
+//  ViewController.swift
 //  PlayList
 //
-//  Created by Xavier Craft on 3/20/17.
-//  Copyright © 2017 craftx17. All rights reserved.
+//  Created by Xavier Craft on 11/30/16.
+//  Copyright © 2016 craftx17. All rights reserved.
+
+// TODO: Base this Tweet ID on some data from elsewhere in your app
 //
 
 import UIKit
 import TwitterKit
 
-class ScoresController: UIViewController {
-
+class ViewController: UIViewController
+{
+    var preferences = [String]()
+    
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    var subjects = ["NFL", "Dallas Cowboys", "New England Patriots", "Oakland Raiders", "New York Giants", "Green Bay Packers", "Seattle Seahawks", "Pittsburg Steelers", "Denver Broncos", "Philadelphia Eagles", "Detroit Lions", "Washington Redskins", "Minnesota Vikings", "San Francisco 49ers", "Houston Texans", "Cleveland Browns", "Atlanta Falcons", "Carolina Panthers", "Kansas City Chiefs", "Baltimore Ravens", "LA Chargers", "Arizona Cardinals", "Chicago Bears", "Buffalo Bills", "Miami Dolphins", "New Orleans Saints", "New York Jets", "Indianapolis Colts", "Tampa Bay Bucaneers", "Cincinatti Bengals", "Tennessee Titans", "Jacksonville Jaguars", "NBA", "Golden State Warriors", "Cleveland Cavaliers", "LA Lakers", "San Antonio Spurs", "New York Knicks", "Chicago Bulls", "Oklahoma City Thunder", "Houston Rockets", "Toronto Raptors", "Boston Celtics", "LA Clippers", "Miami Heat", "New Orleans Pelicans", "Brooklyn Mets", "Milwaukee Bucks", "Philadelphia 76ers", "Dallas Mavericks", "Portland Trail Blazers", "Sacramento Kings", "Atlanta Hawks", "Utah Jazz", "Minnesota Timberwolves", "Washington Wizards", "Detroit Pistons", "Indianapolis Pacers", "Memphis Grizzlies", "Charlotte Hornets", "Phoenix Suns", "Denver Nuggets", "Orlando Magic", "MLB", "Chicago Cubs", "Ney York Yankees", "LA Dodgers", "Toronto Blue Jays", "Cleveland Indians", "Boston Red Sox", "New York Mets", "San Francisco Giants", "St. Louis Cardinals", "Texas Rangers", "Atlanta Braves", "Detroit Tigers", "Baltimore Orioles", "Washington Nationals", "Pittsburg Pirates", "Houston Astros", "Anaheim Angels", "Seattle Mariners", "Philadelphia Phillies", "Kansas City Royals", "Miami Marlins", "Cincinnati Reds", "Minnesota Twins", "San Diego Padres", "Colorado Rockies", "Milwaukee Brewers", "Oakland Athletics", "Arizona Diamondbacks", "Tampa Bay Rays", "Chicago White Sox", "MLS", "Seattle Sounders", "LA Galaxy", "Toronto FC", "Ney York Red Bulls", "New York FC", "Montreal Impact", "Portland Timbers", "Orlando City SC", "Sporting Kansas City", "D.C. United", "Colorado Rapids", "Real Salt Lake", "Houston Dynamo", "Columbus Crew SC", "FC Dallas", "New England Revolution", "Chicago Fire", "San Jose Earthquakes", "Atlanta United FC", "Minnesota United FC", "NHL", "Montreal Canadiens", "Chicago Blackhawks", "Edmonton Oilers", "New York Rangers", "Pittsburg Penguins", "Toronto Maple Leafs", "Boston Bruins", "Vancouver Canucks", "Philadelphia Flyers", "Detroit Red Wings", "Columbus Blue Jackets", "Winnepeg Jets", "Minnesota Wild", "Calgary Flames", "San Jose Sharks", "LA Kings", "St. Louis Blues", "Washington Capitals", "Anaheim Ducks", "Ney York Islanders", "Buffalo Sabres", "Tampa Bay Lightning", "Florida Panthers", "Ottowa Senators", "New Jersey Devils", "Colorado Avalanche", "Carolina Hurricanes", "Arizona Coyotes", "Dallas Stars", "Nashville Predators"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = UIColor.darkGray
         
-        let defaults = UserDefaults.standard
-        let prefs = defaults.value(forKey: "preferences") as! [String]
+        //let defaults = UserDefaults.standard
+        //let result = defaults.value(forKey: "preferences")
+        //print(result!)
+        //preferences = defaults.array(forKey: "preferences") as! [String]
+        
+//        if (preferences.isEmpty) {
+//            performSegue(withIdentifier: "ScoresController", sender: preferences)
+//        }
         
         var index: Int = 0
         var size: Int = 0
         
-        for _ in prefs {
+        for _ in 0...145
+        {
             let button = UIButton()
             button.backgroundColor = UIColor(red: 249/255, green: 249/255, blue: 249/255, alpha: 1.0)
-            button.setTitle(prefs[index], for: .normal)
+            button.setTitle(subjects[index], for: .normal)
             button.setTitleColor(UIColor.darkGray, for: .normal)
             button.addTarget(self, action: #selector(showTimeline), for: .touchUpInside)
+            button.tag = 1
             button.frame = CGRect(x: 0, y: size, width: Int(view.frame.size.width), height: 50)
-            size = size + 51
+            
             index += 1
+            size += 51
+            
             scrollView.addSubview(button)
         }
+    
+        scrollView.backgroundColor = UIColor.darkGray
         
-        print(_preferences)
+        scrollView.contentSize = CGSize(width: view.frame.size.width, height: CGFloat(size))
+        
+    }
+    
+    @IBAction func nextPressed(_ sender: Any){
+        let defaults = UserDefaults.standard
+        defaults.set(preferences, forKey: "preferences")
+        let result = defaults.value(forKey: "preferences")
+        print(result!)
+        performSegue(withIdentifier: "MainController", sender: preferences)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? ScoresController {
+            destination.preferences = preferences
+        }
+    }
+    
+//    func markPrefs() {
+//        if (checkPref(button: button)) { button.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button2)) { button2.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button3)) { button3.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button4)) { button4.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button5)) { button5.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button6)) { button6.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button7)) { button7.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button8)) { button8.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button9)) { button9.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button10)) { button10.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button11)) { button11.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button12)) { button12.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button13)) { button13.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button14)) { button14.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button15)) { button15.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button16)) { button16.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button17)) { button17.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button18)) { button18.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button19)) { button19.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//        
+//        if (checkPref(button: button20)) { button20.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0) }
+//    }
+    
+    func  checkPref(button: UIButton) -> Bool {
+        for pref in preferences {
+            if (button.currentTitle == pref) {
+                return true
+            }
+        }
+        
+        return false
     }
     
     func showTimeline(sender: UIButton!) {
-        // Create an API client and data source to fetch Tweets for the timeline
-        let client = TWTRAPIClient()
-        
-        // Create the timeline view controller
-        let timelineViewController = TWTRTimelineViewController(dataSource: TWTRSearchTimelineDataSource.init(searchQuery: "\(sender.currentTitle!) filter:verified", apiClient: client, languageCode: "en", maxTweetsPerRequest: 500))
-        // Create done button to dismiss the view controller
-        let barButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissTimeline))
-        timelineViewController.navigationItem.leftBarButtonItem = barButton
-        // Create a navigation controller to hold the
-        let navigationController = UINavigationController(rootViewController: timelineViewController)
-        showDetailViewController(navigationController, sender: self)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    @IBAction func changePrefs(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-        _preferences.removeAll()
-        let defaults = UserDefaults.standard
-        defaults.set(_preferences, forKey: "preferences")
-        let result = defaults.value(forKey: "preferences")
-        print(result!)
-    }
-    
-    func dismissTimeline() {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    private var _preferences: [String] = []
-    
-    var preferences: [String] {
-        get { return _preferences }
-        set { _preferences = newValue }
+        preferences.append(sender.currentTitle!)
+        if (sender.backgroundColor == UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0)) {
+            sender.backgroundColor = UIColor(red: 249/255, green: 249/255, blue: 249/255, alpha: 1.0)
+            preferences = preferences.filter { $0 != sender.currentTitle! }
+        }
+        else {
+            sender.backgroundColor = UIColor(red:0.29, green:0.87, blue:0.71, alpha:1.0)
+        }
     }
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
